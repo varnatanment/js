28d30f4567ca4fdd1a2787ce8eb1c2ae59112b90
varnatanment <149891777+varnatanment@users.noreply.github.com>
2023-12-29 13:51:07 +0000

    hard work

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+//Number
+let length = "2.5"
+let weight = "6"
+//strings
+const name = "toyota"
+let model = "1990"
+//boolean
+//either 
+x="true"
+y ="false"
+//object
+const car ="prius"
+const person = {firstName:"John", lastName:"Doe"};
---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId = 123
+let accountemail ="ammar@gmail.com"
+var accountpassword ="321"
+let accountcity ="dera"
+
+accountemail="ama@gmail.com"
+accountpassword="123"
+accountcity="aesa"
+console.table([accountId,accountemail,accountpassword,accountcity])
+
